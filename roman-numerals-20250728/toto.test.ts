1846414a3ac3a941ab1938ddf2ff367b8b1b0a9c
Sébastien Pencréach <sebastien.pencreach.ext@veolia.com>
2025-07-28 13:22:13 +0200

    roman of 1 returns I

--- roman-numerals-20250728/toto.test.ts
+++ roman-numerals-20250728/toto.test.ts
@@ -1,6 +1,9 @@
 import {expect, it} from 'vitest';
-import {toto} from './index';
 
-it('fails', () => {
-  expect(toto()).toBe(false);
+function roman(number: number) {
+  return 'I'
+}
+
+it('roman of 1 returns I', () => {
+  expect(roman(1)).toBe('I');
 })
